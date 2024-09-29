71bd7163b090f43429637d03b62fd95b4a77c797
Mohammadasad098 <mohammadasadiam64@gmail.com>
2024-09-29 16:48:13 +0500

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,36 +11,46 @@ const Navbar = () => {
   return (
     <nav className="border-gray-200 dark:bg-gray-900 bg-[#00b5fd]">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+
+{/* logo start */}
+
         <Link to="/">
           <span className="self-center text-2xl hover:text-white font-semibold whitespace-nowrap dark:text-black text-black">
             Personal Blogging App
           </span>
         </Link>
 
-        <button
-          onClick={toggleMenu}
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-text-black rounded-lg md:hidden hover:bg-text-black focus:outline-none focus:ring-2 focus:ring-text-black dark:text-black dark:hover:bg-text-black dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded={isOpen}
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
+
+{/* logo end */}
+
+
+{/* hamburger start */}
+
+        <label className="btn-circle bg-[#00b5fd] swap swap-rotate md:hidden">
+  <input onClick={toggleMenu} type="checkbox" />
+  <svg
+    className="swap-off fill-current"
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+    viewBox="0 0 512 512">
+    <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+  </svg>
+  <svg
+    className="swap-on fill-current"
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+    viewBox="0 0 512 512">
+    <polygon
+      points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+  </svg>
+</label>
+
+{/* hamburger end */}
+
+
+{/* navbar links start */}
 
         <div
           className={`${isOpen ? "block" : "hidden"} w-full md:block md:w-auto`}
@@ -97,6 +107,10 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
+
+
+{/* navbar links end */}
+
       </div>
     </nav>
   );
