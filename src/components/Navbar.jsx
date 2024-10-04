4e581de21df40a93c595a5e3bb067989ccf55049
Mohammadasad098 <mohammadasadiam64@gmail.com>
2024-10-04 20:43:37 +0500

    updating

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -83,7 +83,7 @@ const Navbar = () => {
           className={`${isOpen ? "block" : "hidden"} w-full md:block md:w-auto`}
           id="navbar-default"
         >
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
             {["/", "/dashboard", "/profile", "/login", "/register", "/logout"].map((path, index) => (
               <li key={index}>
                 <Link
